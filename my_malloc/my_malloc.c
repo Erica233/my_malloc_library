06a608985bbdb16156a3a92ebc4e01ee9f84d2ec
Fangting Ma <fm128@duke.edu>
2022-01-17 16:13:29 -0500

    1

--- my_malloc/my_malloc.c
+++ my_malloc/my_malloc.c
@@ -2,15 +2,33 @@
 #include <stdio.h>
 #include "my_malloc.h"
 
+typedef struct block_info block_info_t;
+struct block_info {
+  int available;
+  size_t size;
+  block_info_t *next;
+};
+#define BLOCK_SIZE sizeof(block_info_t)
+block_info_t *head = NULL;
+block_info_t *tail = NULL;
+
+void *ff_malloc(size_t size);
+void ff_free(void *ptr);
+
+
 int main() {
     printf("hello");
     return 0;
 }
 
-/*
 //First Fit malloc
 void *ff_malloc(size_t size) {
-
+  // if there is no available block, then call sbrk() to create
+  // first_block is null, or free_list is empty, or blocks in free_list are all smaller than required
+  if (head == NULL) {
+    sbrk(size + );
+  }
+  
 }
 
 //First Fit free
@@ -18,6 +36,7 @@ void ff_free(void *ptr) {
   
 }
 
+/*
 //Best Fit malloc
 void *bf_malloc(size_t size) {
 
