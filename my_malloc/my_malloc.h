e0798db3a9a7fe2bb0c2d002eab5073ddf2a252f
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-18 16:21:10 -0500

    1

--- my_malloc/my_malloc.h
+++ my_malloc/my_malloc.h
@@ -16,4 +16,7 @@ void ff_free(void *ptr);
 void *bf_malloc(size_t size);
 void bf_free(void *ptr);
 
+unsigned long get_data_segment_size();
+unsigned long get_data_segment_free_space_size();
+
 #endif
