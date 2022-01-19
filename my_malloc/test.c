eef2d091ba4adf472e8b6d8241e4529d1615ab62
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-19 07:05:26 -0500

    mod & test

--- my_malloc/test.c
+++ my_malloc/test.c
@@ -6,6 +6,7 @@ int main() {
     printf("%p ", sbrk(0));
     metadata_t * curr = sbrk(METADATA_SIZE);
     printf("%p\n", sbrk(0));
+    printf("addr %p\n", (void *)curr);
     printf("\n\n----------test result----------\n\n\n");
 
 
