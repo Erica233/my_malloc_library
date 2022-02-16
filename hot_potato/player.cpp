a4ff1033d0f5e12567ec352d6ed4a5c042e94714
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-02-16 17:08:19 -0500

    connect neighbor - host

--- hot_potato/player.cpp
+++ hot_potato/player.cpp
@@ -52,7 +52,7 @@ int main(int argc, char **argv) {
     int right_port;
     std::string right_host;
     recv(socket_fd, &right_port, sizeof(right_port), 0);
-    recv(socket_fd, &right_host, sizeof(right_host), 0);
+    recv(socket_fd, &right_host, right_host.size(), 0);
     std::cout << "sizeof(right_host): " << sizeof(right_host) << std::endl;
     std::cout << "right_id: " << right_id << std::endl;
     std::cout << "right_port: " << right_port << std::endl;
