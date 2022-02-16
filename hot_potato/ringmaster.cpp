fdad001ae0ab67e73b820b06e5ef9d998fd4ef57
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-02-16 15:01:32 -0500

    connect neighbor - host

--- hot_potato/ringmaster.cpp
+++ hot_potato/ringmaster.cpp
@@ -46,9 +46,9 @@ int main(int argc, char **argv) {
             return EXIT_FAILURE;
         }
         std::cout << "inet_ntoa: " << inet_ntoa(((struct sockaddr_in *)&socket_addr)->sin_addr) << std::endl;
-        std::string host;
-        inet_ntop(AF_INET, &(((struct sockaddr_in *)&socket_addr)->sin_addr), &host, INET_ADDRSTRLEN);
-        std::cout << "host: " << host << std::endl;
+        std::string * host;
+        inet_ntop(AF_INET, &(((struct sockaddr_in *)&socket_addr)->sin_addr), host, INET_ADDRSTRLEN);
+        std::cout << "host: " << *host << std::endl;
         //ips.push_back(host);
 
         send(client_connect_fd, &i, sizeof(i), 0);
